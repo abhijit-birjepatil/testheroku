1244bd3e2f5e5d9bb611990ee74da380fbd93461
abhijit <abhijit@comitydesigns.com>
2017-01-09 23:27:52 +0530

    na

--- javascripts/nforceoauth.js
+++ javascripts/nforceoauth.js
@@ -3,7 +3,7 @@ var nforce = require('nforce');
 global.org = nforce.createConnection({
   clientId: process.env.APPID,
   clientSecret: process.env.APPKEY,
-  redirectUri: 'http://localhost:6000/oauthcallback',
+  redirectUri: 'https://abhijitfirstapp.herokuapp.com/oauthcallback',
   mode: 'single',
   autoRefresh: true // <--- set this to true
 });
