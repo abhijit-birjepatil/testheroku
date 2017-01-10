0ce831871210b0509480578d0b36ee7fae3f610c
abhijit <abhijit@comitydesigns.com>
2017-01-10 17:07:48 +0530

    na

--- app.js
+++ app.js
@@ -2,16 +2,10 @@
 var express = require('express');
 var oauth = require('./lib/oAuth/oauth');
 var port = process.env.PORT || 3000;
-var env = require('node-env-file');
 
 var app = express();
 
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false })); //app.use(app.router);
-
-// Load environment variables for localhost
-try {
-	env(__dirname + '/process.env');
-} catch (e) {console.log("error ", e);}
 
 
 // Require Routes js
